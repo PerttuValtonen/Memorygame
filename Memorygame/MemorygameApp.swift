433da901faec75f79da3ab3f7935dbd43ac2a60e
PerttuValtonen <135586722+PerttuValtonen@users.noreply.github.com>
2023-06-12 11:32:30 +0300

    Initial Commit

---
+++ Memorygame/MemorygameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorygameApp.swift
+//  Memorygame
+//
+//  Created by Perttu Valtonen on 12.6.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorygameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
